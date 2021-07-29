c4fa42ff1be722a6fdbac0d4c58b73fddd2b0d2e
Anderson <andersoncleyson@protonmail.com>
2021-07-29 08:10:20 -0300

    Criando use case de categoria

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,28 @@
-class CreateSpecificationService {
+import { ISpecificationRepository } from "../repositories/ISpecificatiosRepository";
 
-    execute(){}
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+
+class CreateSpecificationService {
+    constructor(private specificationsRepository: ISpecificationRepository){}
+    execute({name, description}: IRequest): void {
+
+        const specificationAlreadyExists = this.specificationsRepository.findByName(
+            name
+        )
+
+        if (specificationAlreadyExists){
+            throw new Error("Specification already exists!")
+        }
+
+        this.specificationsRepository.create({
+            name,
+            description,
+        });
+    }
 }
 
 export {CreateSpecificationService}
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,12 @@
 import { Router } from 'express';
-import { PostgresCategoriesRepository } from '../repositories/PostfgreesCategoryRepository';
-import { CreateCategoryService } from '../services/CreateCategoryService';
+import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
+import { createCategoryController } from '../modules/cars/useCases/createCatogory';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new PostgresCategoriesRepository()
+const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post("/", (request, response) => {
-    const { name, description } = request.body
-
-    const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-    createCategoryService.execute({name, description});
-
-    return response.status(201).send()
+    return createCategoryController.handle(request, response)
 });
 
 categoriesRoutes.get('/', (request, response) => {
